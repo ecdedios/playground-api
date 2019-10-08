f9246596c514278f262f5ebc39378577217068df
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-10-07 19:58:16 -0500

    add error

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hi')
 def index():
     return "The server works!"
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/hello', methods=['GET','POST'])
 def hello():
     if request.method == 'POST':
         return request.data
